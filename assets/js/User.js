31e2bdd08c308bff9078164982553c3792bc1a66
Ricop2705 <ricop.1802@gmail.com>
2026-03-23 18:43:49 +0900

    all document accept

--- assets/js/User.js
+++ assets/js/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
@@ -16,23 +15,4 @@ const UserSchema = new mongoose.Schema({
 });
 
 export default mongoose.models.User ||
-=======
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: {
-    type: String,
-    unique: true
-  },
-  phone: String,
-  password: String,
-  createdAt:{
-    type: Date,
-    default: Date.now
-  }
-});
-
-export default mongoose.models.User ||
->>>>>>> 1e59d521d95e2963f38720f5b1509d18194af5f3
        mongoose.model("User", UserSchema);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
 
   const loginBtn = document.querySelector("#loginBtn");
@@ -10,16 +9,3 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-
-  const loginBtn = document.querySelector("#loginBtn");
-
-  if (loginBtn) {
-    loginBtn.addEventListener("click", () => {
-      window.location.href = "/auth.html";
-    });
-  }
-
-});
->>>>>>> 1e59d521d95e2963f38720f5b1509d18194af5f3
